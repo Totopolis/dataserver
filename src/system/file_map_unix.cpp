18a4b3544b0623649e8ef7de206d70ad7354f0ef
Ivan Dalidchik <idalidchik@gmail.com>
2015-12-14 14:41:04 +0300

    select mmap64 if available

--- src/system/file_map_unix.cpp
+++ src/system/file_map_unix.cpp
@@ -1,15 +1,51 @@
 // file_map_unix.cpp
 //
-#include "common/common.h"
-#include "file_map_detail.h"
-#include <utility>
-
 #if defined(SDL_OS_UNIX)
 
+#include "common/common.h"
+#include "file_map_detail.h"
 #include "file_h.h"
 #include <sys/mman.h>
+#include <utility>
 
-namespace sdl {
+namespace sdl { namespace {
+
+struct substitution_failure {}; // represent a failure to declare something
+
+template<typename T> 
+struct substitution_succeeded : std::true_type {};
+        
+template<>
+struct substitution_succeeded<substitution_failure> : std::false_type {};
+
+struct get_mmap64 {
+private:
+    template<typename X>
+    static auto check(X const& x) -> decltype(mmap64(x, 0, 0, 0, 0, 0));
+    static substitution_failure check(...);
+public:
+    using type = decltype(check(nullptr));
+};
+
+struct has_mmap64: substitution_succeeded<get_mmap64::type> {};
+
+template<bool>
+struct select_mmap64 {
+    template<typename... Values>
+    static void * get(Values&&... params) {
+        return mmap(std::forward<Values>(params)...);
+    }
+};
+
+template<>
+struct select_mmap64<true> {
+    template<typename... Values>
+    static void * get(Values&&... params) {
+        return mmap64(std::forward<Values>(params)...);
+    }
+};
+
+} // namespace
 
 file_map_detail::view_of_file 
 file_map_detail::map_view_of_file(const char* filename,
@@ -30,11 +66,15 @@ file_map_detail::map_view_of_file(const char* filename,
             SDL_ASSERT(false);
             return nullptr;
         }
-        //FIXME: mmap64 on Linux ?
-        auto pFileView = ::mmap(0, 
+#if 0
+        auto pFileView = ::mmap(nullptr, 
             static_cast<size_t>(size), 
             PROT_READ, MAP_PRIVATE, fileno(fp.get()), 0);
-
+#else
+        auto pFileView = select_mmap64<has_mmap64::value>::get(
+            nullptr, static_cast<size_t>(size), 
+            PROT_READ, MAP_PRIVATE, fileno(fp.get()), 0);
+#endif
         if (pFileView == MAP_FAILED) {
             SDL_TRACE_2("mmap failed: ", filename);
             SDL_ASSERT(false);
@@ -66,32 +106,12 @@ bool file_map_detail::unmap_view_of_file(
 #if SDL_DEBUG
 namespace sdl { namespace {
 
-struct substitution_failure {}; // represent a failure to declare something
-
-template<typename T> 
-struct substitution_succeeded : std::true_type {};
-        
-template<>
-struct substitution_succeeded<substitution_failure> : std::false_type {};
-
-template<typename T>
-struct get_mmap64 {
-private:
-    template<typename X>
-    static auto check(X const& x) -> decltype(mmap64(x, 0, 0, 0, 0, 0));
-    static substitution_failure check(...);
-public:
-    using type = decltype(check(std::declval<T>()));
-};
-
-struct has_mmap64: substitution_succeeded<get_mmap64<decltype(nullptr)>::type> {};
-
 class unit_test {
 public:
     unit_test()
     {
         SDL_TRACE(__FILE__);
-        SDL_TRACE_2("has_mmap64 = ", has_mmap64::value);
+        SDL_TRACE_2("has_mmap64::value = ", has_mmap64::value);
     }
 };
 static unit_test s_test;
