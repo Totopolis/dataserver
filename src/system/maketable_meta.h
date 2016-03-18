3bd01aa21ea03dadf2b04ded791279d13f8173bc
idalidchik <idalidchik@gmail.com>
2016-03-18 23:21:50 +0300

    code clean up

--- src/system/maketable_meta.h
+++ src/system/maketable_meta.h
@@ -160,22 +160,6 @@ bool check_cluster_index() {
     return _check_cluster_index<T>::check();
 }
 
-#if 0
-template<class TList, size_t> struct processor;
-template<size_t index> struct processor<NullType, index> {
-    template<class fun_type>
-    static void apply(fun_type){}
-};
-template <class T, class U, size_t index>
-struct processor<Typelist<T, U>, index> {
-    template<class fun_type>
-    static void apply(fun_type fun){
-        //FIXME: simplify code for linux build
-        //FIXME: fun(identity<T>(), Int2Type<index>());
-        processor<U, index + 1>::apply(fun);
-    }
-};
-#else
 template<class TList> struct processor;
 template<> struct processor<NullType> {
     template<class fun_type>
@@ -189,7 +173,6 @@ struct processor<Typelist<T, U>> {
         processor<U>::apply(fun);
     }
 };
-#endif
 
 } // meta
 } // make
