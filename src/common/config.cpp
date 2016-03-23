41bd11f2a421afc3125821eeaddf6ebdd89376a6
Ivan Dalidchik <idalidchik@gmail.com>
2016-03-23 15:54:22 +0300

    doc updated

--- src/common/config.cpp
+++ src/common/config.cpp
@@ -9,6 +9,7 @@ int debug::warning_level = 1;
 void debug::warning(const char * message, const char * fun, const int line) {
     if (warning_level) {
         std::cout << "\nwarning (" << message << ") in " << fun << " at line " << line << std::endl; 
+        SDL_ASSERT(warning_level < 2);
     }
 }
 } // sdl
