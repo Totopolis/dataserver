56d73b3afbfff3198862c7a31a1acf72a85ae7af
Ivan Dalidchik <idalidchik@gmail.com>
2016-09-13 21:34:51 +0300

    todo: merge cells

--- dataserver/spatial/interval_cell.h
+++ dataserver/spatial/interval_cell.h
@@ -122,47 +122,70 @@ interval_cell::for_interval(const_iterator it, fun_type const & fun) const
         SDL_ASSERT(!is_interval(*it));
         SDL_ASSERT(it != m_set->end());
         const uint32 x2 = (it++)->r32();
-#if 0 //FIXME: merge cells
+#if 0 //FIXME: merge cells, prototype
         static_assert(cell_capacity<4>::upper_bound == 0xFF, "");
         static_assert(cell_capacity<4>::value == 0x100, "");
         if (x2 >= x1 + 0xFF) {
-            if (x1 & 0xFF) {
-                const uint32 x11 = (x1 & ~uint32(0xFF)) + 0x100;
-                if (x2 >= x11 + 0xFF) {
-                    const uint32 count = uint64(x2 - x11 + 1) / 0xFF;
-                    const uint32 x22 = x11 + count * 0x100 - 1;
-                    SDL_ASSERT(x1 <= x11);
-                    SDL_ASSERT(x11 < x22);
-                    SDL_ASSERT(x22 <= x2);
-#if 0 // SDL_DEBUG > 1
-                    std::cout << "interval_cell:" << std::hex
-                        << "x1 = " << x1
-                        << ",x11 = " << x11
-                        << ",x22 = " << x22
-                        << ",x2 = " << x2 << std::dec
-                        << ",count = " << count << std::endl;
-#endif
-                    for (uint32 x = x1; x < x11; ++x) {
-                        if (make_break_or_continue(fun(
-                            spatial_cell::init(reverse_bytes(x), 4))) == bc::break_) {
-                            return { it, bc::break_ }; 
-                        }
+            const uint32 x11 = (x1 & ~uint32(0xFF)) + 0x100;
+            if (x2 >= x11 + 0xFF) {
+                const uint32 count11 = uint64(x2 - x11 + 1) / 0x100;
+                const uint32 x22 = x11 + count11 * 0x100 - 1;
+                SDL_ASSERT(x1 <= x11);
+                SDL_ASSERT(x11 < x22);
+                SDL_ASSERT(x22 <= x2);
+                for (uint32 x = x1; x < x11; ++x) {
+                    if (make_break_or_continue(fun(
+                        spatial_cell::init(reverse_bytes(x), 4))) == bc::break_) {
+                        return { it, bc::break_ }; 
                     }
-                    for (uint32 x = x11; x < x22; x += 0x100) {
-                        SDL_ASSERT(!(x & 0xFF));
-                        if (make_break_or_continue(fun(
-                            spatial_cell::init(reverse_bytes(x), 3))) == bc::break_) {
-                            return { it, bc::break_ }; 
-                        }
-                    }
-                    for (uint32 x = x22 + 1; x <= x2; ++x) {
-                        if (make_break_or_continue(fun(
-                            spatial_cell::init(reverse_bytes(x), 4))) == bc::break_) {
-                            return { it, bc::break_ }; 
-                        }
-                    }
-                    return { it, bc::continue_ };
                 }
+                if (x22 >= x11 + 0xFFFF) {
+                    const uint32 x111 = (x11 & ~uint32(0xFFFF)) + 0x10000;
+                    if (x22 >= x111 + 0xFFFF) {
+                        const uint32 count111 = uint64(x22 - x111 + 1) / 0x10000;
+                        const uint32 x222 = x111 + count111 * 0x10000 - 1;
+                        SDL_ASSERT(x11 <= x111);
+                        SDL_ASSERT(x111 < x222);
+                        SDL_ASSERT(x222 <= x22);
+                        for (uint32 x = x11; x < x111; x += 0x100) {
+                            SDL_ASSERT(!(x & 0xFF));
+                            if (make_break_or_continue(fun(
+                                spatial_cell::init(reverse_bytes(x), 3))) == bc::break_) {
+                                return { it, bc::break_ }; 
+                            }
+                        }
+                        for (uint32 x = x111; x < x222; x += 0x10000) {
+                            SDL_ASSERT(!(x & 0xFFFF));
+                            if (make_break_or_continue(fun(
+                                spatial_cell::init(reverse_bytes(x), 2))) == bc::break_) {
+                                return { it, bc::break_ }; 
+                            }
+                        }
+                        for (uint32 x = x222 + 1; x < x22; x += 0x100) {
+                            SDL_ASSERT(!(x & 0xFF));
+                            if (make_break_or_continue(fun(
+                                spatial_cell::init(reverse_bytes(x), 3))) == bc::break_) {
+                                return { it, bc::break_ }; 
+                            }
+                        }
+                        goto continue_;
+                    }
+                }
+                for (uint32 x = x11; x < x22; x += 0x100) {
+                    SDL_ASSERT(!(x & 0xFF));
+                    if (make_break_or_continue(fun(
+                        spatial_cell::init(reverse_bytes(x), 3))) == bc::break_) {
+                        return { it, bc::break_ }; 
+                    }
+                }
+continue_:
+                for (uint32 x = x22 + 1; x <= x2; ++x) {
+                    if (make_break_or_continue(fun(
+                        spatial_cell::init(reverse_bytes(x), 4))) == bc::break_) {
+                        return { it, bc::break_ }; 
+                    }
+                }
+                return { it, bc::continue_ };
             }
         }
 #endif
@@ -180,6 +203,15 @@ interval_cell::for_interval(const_iterator it, fun_type const & fun) const
         return { ++it, b };
     }
 }
+
+#if 0 // SDL_DEBUG > 1
+                    std::cout << "interval_cell:" << std::hex
+                        << "x1 = " << x1
+                        << ",x11 = " << x11
+                        << ",x22 = " << x22
+                        << ",x2 = " << x2 << std::dec
+                        << ",count = " << count << std::endl;
+#endif
 
 template<class fun_type>
 break_or_continue interval_cell::for_each(fun_type const & fun) const
