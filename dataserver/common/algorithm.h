3af021254e53afd4fa777912e911f1d3b9130358
Ivan Dalidchik <idalidchik@gmail.com>
2016-10-07 12:31:48 +0300

    ASSERT_SCOPE_EXIT

--- dataserver/common/algorithm.h
+++ dataserver/common/algorithm.h
@@ -6,25 +6,85 @@
 
 #include <algorithm>
 
-namespace sdl { namespace algo {
+namespace sdl { namespace algo { namespace scope_exit { 
+
+template<typename fun_type>
+struct scope_guard {
+    explicit scope_guard(fun_type && f) : fun(std::move(f)) {}
+    ~scope_guard() {
+        fun();
+    }
+private:
+    fun_type fun;
+};
+
+template <typename T> inline
+scope_guard<T> create_scope_guard(T&& exitScope) {
+    return scope_guard<T>(std::forward<T>(exitScope));
+}
+
+#if SDL_DEBUG
+template<typename fun_type>
+struct assert_guard {
+    explicit assert_guard(fun_type && f) : fun(std::move(f)) {}
+    ~assert_guard() { 
+        SDL_ASSERT(fun());
+    }
+private:
+    fun_type fun;
+};
+#else
+template<typename fun_type>
+struct assert_guard {
+    explicit assert_guard(fun_type &&){}
+};
+#endif
+
+template <typename T> inline
+assert_guard<T> create_assert_guard(T&& exitScope) {
+    return assert_guard<T>(std::forward<T>(exitScope));
+}
+
+} // scope_exit
+
+#define _SDL_UTILITY_EXIT_SCOPE_LINENAME_CAT(name, line) name##line
+#define _SDL_UTILITY_EXIT_SCOPE_LINENAME(name, line) _SDL_UTILITY_EXIT_SCOPE_LINENAME_CAT(name, line)
+
+#define SDL_UTILITY_SCOPE_EXIT(f) const auto& _SDL_UTILITY_EXIT_SCOPE_LINENAME(scope_exit_line_, __LINE__) = scope_exit::create_scope_guard(f)
+#define SDL_UTILITY_ASSERT_EXIT(f) const auto& _SDL_UTILITY_EXIT_SCOPE_LINENAME(scope_exit_line_, __LINE__) = scope_exit::create_assert_guard(f)
+
+#if SDL_DEBUG
+#define ASSERT_SCOPE_EXIT(...)  SDL_UTILITY_ASSERT_EXIT(__VA_ARGS__)
+#else
+#define ASSERT_SCOPE_EXIT(...)  ((void)0)
+#endif
+
+#if SDL_DEBUG > 1
+#define ASSERT_SCOPE_EXIT_DEBUG_2(...)  ASSERT_SCOPE_EXIT(__VA_ARGS__)
+#else
+#define ASSERT_SCOPE_EXIT_DEBUG_2(...)  ((void)0)
+#endif
 
 template<class T, class key_type>
 inline bool is_find(T const & result, key_type const & value) {
-    return std::find(result.begin(), result.end(), value) != result.end();
+    return std::find(std::begin(result), std::end(result), value) != std::end(result);
 }
 
 template<class T>
 inline bool is_sorted(T const & result) {
-    return std::is_sorted(result.cbegin(), result.cend());
+    return std::is_sorted(std::cbegin(result), std::cend(result));
 }
 
 template<class T, class fun_type>
 inline bool is_sorted(T const & result, fun_type compare) {
-    return std::is_sorted(result.cbegin(), result.cend(), compare);
+    return std::is_sorted(std::cbegin(result), std::cend(result), compare);
 }
 
 template<class T, class key_type>
 bool binary_insertion(T & result, key_type && unique_key) {
+    ASSERT_SCOPE_EXIT_DEBUG_2([&result]{
+        return is_sorted(result);
+    });
     if (!result.empty()) {
         const auto pos = std::lower_bound(result.begin(), result.end(), unique_key);
         if (pos != result.end()) {
@@ -36,12 +96,14 @@ bool binary_insertion(T & result, key_type && unique_key) {
         }
     }
     result.push_back(std::forward<key_type>(unique_key)); 
-    SDL_ASSERT_DEBUG_2(is_sorted(result));
     return true;
 }
 
 template<class T, class key_type, class fun_type>
 bool binary_insertion(T & result, key_type && unique_key, fun_type compare) {
+    ASSERT_SCOPE_EXIT_DEBUG_2([&result, compare]{
+        return is_sorted(result, compare);
+    });
     if (!result.empty()) {
         const auto pos = std::lower_bound(result.begin(), result.end(), unique_key, compare);
         if (pos != result.end()) {
@@ -50,17 +112,18 @@ bool binary_insertion(T & result, key_type && unique_key, fun_type compare) {
                 return false;
             }
             result.insert(pos, std::forward<key_type>(unique_key));
-            SDL_ASSERT_DEBUG_2(is_sorted(result, compare));
             return true;
         }
     }
     result.push_back(std::forward<key_type>(unique_key)); 
-    SDL_ASSERT_DEBUG_2(is_sorted(result, compare));
     return true;
 }
 
 template<class T, class key_type>
 void insertion_sort(T & result, const key_type & value) {
+    ASSERT_SCOPE_EXIT_DEBUG_2([&result]{
+        return is_sorted(result);
+    });
     result.push_back(value);
     auto const left = result.begin();
     auto right = result.end(); --right;
@@ -74,11 +137,13 @@ void insertion_sort(T & result, const key_type & value) {
         }
     }
     SDL_ASSERT(left <= right);
-    SDL_ASSERT_DEBUG_2(is_sorted(result));
 }
 
 template<class T, class key_type, class fun_type>
 void insertion_sort(T & result, const key_type & value, fun_type compare) {
+    ASSERT_SCOPE_EXIT_DEBUG_2([&result, compare]{
+        return is_sorted(result, compare);
+    });
     result.push_back(value);
     auto const left = result.begin();
     auto right = result.end(); --right;
@@ -92,7 +157,6 @@ void insertion_sort(T & result, const key_type & value, fun_type compare) {
         }
     }
     SDL_ASSERT(left <= right);
-    SDL_ASSERT_DEBUG_2(is_sorted(result, compare));
 }
 
 } // algo
