d5a0031d347c7d851eec680f5c757cd0fb885e44
Ivan Dalidchik <idalidchik@gmail.com>
2016-12-27 13:12:41 +0300

    SDL_TRACE_ERROR

--- dataserver/common/config.h
+++ dataserver/common/config.h
@@ -75,6 +75,12 @@ inline void SDL_ASSERT_1(bool x)    { assert(x); }
 #define SDL_ASSERT_WIN32(...)       ((void)0)
 #endif
 
+#if SDL_DEBUG
+#define SDL_TRACE_ERROR(...)        (SDL_TRACE(__VA_ARGS__), SDL_ASSERT_WIN32(false), 0)
+#else
+#define SDL_TRACE_ERROR(...)        ((void)0)
+#endif
+
 #define CURRENT_BYTE_ORDER          (*(uint32 *)"\x01\x02\x03\x04")
 #define LITTLE_ENDIAN_BYTE_ORDER    0x04030201
 #define BIG_ENDIAN_BYTE_ORDER       0x01020304
