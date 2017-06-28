5b23c286f0cec234d6edd71d76cb09c92bb1df63
Ivan Dalidchik <idalidchik@gmail.com>
2017-06-28 14:08:12 +0300

    todo: vm_alloc

--- dataserver/system/memory/address_space.cpp
+++ dataserver/system/memory/address_space.cpp
@@ -2,9 +2,53 @@
 //
 #include "dataserver/system/memory/address_space.h"
 
-namespace sdl { namespace db {
+#if defined(SDL_OS_WIN32)
+#endif
 
+namespace sdl { namespace db { namespace mmu {
 
+vm_alloc::vm_alloc(uint64 const size)
+    : m_reserved(size)
+{
+    SDL_ASSERT(size && !(size % page_size));
+}
+
+vm_alloc::~vm_alloc()
+{
+}
+
+void * vm_alloc::alloc(uint64 const start, uint64 const size)
+{
+    SDL_ASSERT(start + size <= m_reserved);
+    SDL_ASSERT(size && !(size % page_size));
+    SDL_ASSERT(!(start % page_size));
+    return nullptr;
+}
+
+#if SDL_DEBUG
+namespace {
+    class unit_test {
+    public:
+        unit_test() {
+            using T = address_translation;
+            if (1) {
+                T test;
+                SDL_ASSERT(test[0] == 0);
+                SDL_ASSERT(test[test.size() - 1] == 0);
+                test.page(0) = 1;
+                SDL_ASSERT(test[0] == 1);
+            }
+            if (1) {
+                vm_alloc test(page_head::page_size);
+                auto p = test.alloc(0, test.reserved());
+            }
+        }
+    };
+    static unit_test s_test;
+}
+#endif //#if SDL_DEBUG
+
+} // mmu
 } // sdl
 } // db
 
--- dataserver/system/memory/address_space.h
+++ dataserver/system/memory/address_space.h
@@ -6,11 +6,42 @@
 
 #include "dataserver/system/page_head.h"
 
-namespace sdl { namespace db {
+namespace sdl { namespace db { namespace mmu {
 
-struct address_translation {
+class address_translation : noncopyable {
+    enum { page_size = page_head::page_size }; // 8192 byte (8 kilobyte)
+    enum { max_page = 1 << 16 }; // 65536 pages
+    using pagetable = array_t<uint16, max_page>; // 8192 * 65536 = 536870912 bytes address space
+    static_assert(sizeof(pagetable) == 65536 * sizeof(uint16), ""); // 131072 bytes
+    static_assert(max_page - 1 == uint16(-1), "");
+    const std::unique_ptr<pagetable> tbl;
+public:
+    address_translation() : tbl(new pagetable){
+        tbl->fill_0();
+    }
+    static constexpr size_t size() { return max_page; }
+    uint16 operator[](size_t const i) const noexcept {
+        SDL_ASSERT(i < size());
+        return (*tbl)[i];
+    }
+    uint16 & page(size_t const i) noexcept {
+        SDL_ASSERT(i < size());
+        return (*tbl)[i];
+    }
 };
 
+class vm_alloc : noncopyable {
+    enum { page_size = page_head::page_size }; // 8192 byte
+public:
+    explicit vm_alloc(uint64);
+    ~vm_alloc();
+    uint64 reserved() const { return m_reserved; }
+    void * alloc(uint64 start, uint64 size);
+private:
+    uint64 const m_reserved;
+};
+
+} // mmu
 } // db
 } // sdl
 
