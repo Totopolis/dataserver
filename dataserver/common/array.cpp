c76969a15ca0e84ef477e6f0dc0723917d2eafc0
Ivan Dalidchik <idalidchik@gmail.com>
2017-10-03 10:38:48 +0300

    unit test

--- dataserver/common/array.cpp
+++ dataserver/common/array.cpp
@@ -1,3 +1,25 @@
 // array.cpp
 //
 #include "dataserver/common/array.h"
+
+
+#if SDL_DEBUG
+namespace sdl { namespace {
+    class unit_test {
+    public:
+        unit_test() {
+            {
+                using T = array_t<int, 10>;
+                static_assert(sizeof(T) == sizeof(T::elems_t), "");
+                A_STATIC_ASSERT_IS_POD(T);
+            }
+            {
+                using T = array_t<std::string, 10>;
+                static_assert(sizeof(T) == sizeof(T::elems_t), "");
+            }
+        }
+    };
+    static unit_test s_test;
+}
+} // sdl
+#endif //#if SDL_DEBUG
