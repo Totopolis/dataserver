a5d1e8e9c1ca2146da3b3bd6fb5d5d78502617cd
idalidchik <idalidchik@gmail.com>
2017-12-08 11:57:50 +0300

    long_long_unsigned

--- dataserver/common/static.h
+++ dataserver/common/static.h
@@ -25,6 +25,7 @@ using uint32 = std::uint32_t;
 using int64 = std::int64_t;
 using uint64 = std::uint64_t;
 typedef long long long_long;
+typedef long long unsigned long_long_unsigned;
 
 struct is_32_bit { enum { value = (sizeof(void *) == sizeof(std::uint32_t)) }; };
 struct is_64_bit { enum { value = (sizeof(void *) == sizeof(std::uint64_t)) }; };
