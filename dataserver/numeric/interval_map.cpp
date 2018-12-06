b12f7c3f4d177b107d3a96b15535d030dfb387e4
idalidchik <idalidchik@gmail.com>
2018-12-06 17:59:01 +0300

    interval_map.cpp

--- dataserver/numeric/interval_map.cpp
+++ dataserver/numeric/interval_map.cpp
@@ -18,8 +18,6 @@ static unit_test s_test;
 
 void unit_test::test1()
 {
-    SDL_TRACE_FUNCTION;
-
 	typedef int key_t;
 	typedef int val_t;
 	typedef interval_map<key_t, val_t> map_type;
@@ -28,56 +26,32 @@ void unit_test::test1()
 	enum { N = 111 };
 	enum { M = 1000 };
 	enum { M2 = 1 };
-	for (int j = 0; j < M; ++j)
-	for (int j2 = 0; j2 < M2; ++j2)
-	{
+	for (int j = 0; j < M; ++j) {
+	for (int j2 = 0; j2 < M2; ++j2)	{
 		typedef std::vector<key_t> vector_key_t;
 		typedef std::vector<val_t> vector_val_t;
 		vector_key_t dkeyBegin, dkeyEnd;
 		vector_val_t dval;
-		const bool print = (0 == j) && (0 == j2);
-		if (print) { 
-			std::cout << "assign:\n";
-		}
-		for (int i = 0; i < N; ++i)
-		{
+		for (int i = 0; i < N; ++i) {
 			const key_t keyBegin = rand() % 100;
 			const key_t keyEnd = keyBegin + rand() % 100;
 			const val_t val = rand() % 100;
 			dkeyBegin.push_back(keyBegin);
 			dkeyEnd.push_back(keyEnd);
 			dval.push_back(val);
-			if (print) {
-				std::cout << i << ": (" << keyBegin << "," << keyEnd << "," << val << ")\n";
-			}
 			test.assign(keyBegin, keyEnd, val);
 		}
-		if (print) {
-			std::cout << "trace map:\n";
-		}
-		auto it = test.map().begin();
-		for (; it != test.map().end(); ++it) {
-			if (print) {
-				std::cout
-					<< "key = " << it->first << ", "
-					<< "val = " << it->second << "\n";
-			}
-			if (it != test.map().begin()) {
+        if (!test.map().empty()) {
+            const auto end = test.map().end();
+		    auto it = test.map().begin();
+            ++it;
+		    for (; it != end; ++it) {
 				auto it2 = it; --it2;
-				if (it2->second == it->second) // error !
-				{
-					map_type test2(-1);
-					for (size_t k = 0; k < dkeyBegin.size(); ++k)
-					{
-						std::cout << k << ": (" << dkeyBegin[k] << "," << dkeyEnd[k] << "," << dval[k] << ")\n";
-						test2.assign(dkeyBegin[k], dkeyEnd[k], dval[k]);
-					}
-				}
-				assert(it2->second != it->second);
-				assert(it2->first < it->first);
-			}
-		}
-	}
+				SDL_ASSERT(it2->second != it->second);
+				SDL_ASSERT(it2->first < it->first);
+		    }
+        }
+    }}
 }
 
 }} // sdl
